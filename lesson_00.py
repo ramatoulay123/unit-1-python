89d918499e9f6ebeab5cbabd44c7583e71db8aff
ramatoulay123 <rbarry@brooklynsteamcenter.org>
2024-09-16 10:52:32 -0400

    new

---
+++ lesson_00.py
@@ -0,0 +1,45 @@
+
+# 1st step 
+name_show = "supernatural"  
+# 2nd step 
+x = 0.5 
+y = 4
+print(name_show)
+# 5th step 
+print(x * y)
+# step 6 
+favorite_food = ["pasta", "pizza","chicken"]   
+print(favorite_food)     
+# step 7 
+age = 16 
+if age > 16: 
+    print("you can get your driver license")
+
+else: 
+    print("You need to wait")
+
+# step 8
+name = input() 
+
+print(name)
+
+# step 9
+favorite_food = ["pasta", "pizza","chicken"]  
+for x in favorite_food: 
+     
+    print(x)       
+
+# step 10 
+
+i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    break
+  i += 1
+
+
+
+
+
+
