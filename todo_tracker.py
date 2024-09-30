35fd16f33cc732687908ffbffdd584468ac467f4
ramatoulay123 <rbarry@brooklynsteamcenter.org>
2024-09-30 10:41:44 -0400

    to tracker

--- todo_tracker.py
+++ todo_tracker.py
@@ -1,5 +1,9 @@
 
 todos = ["get cash", "spend cash", "cry"]
+# it oprns the file and reads it line by line
+with open("todo_tracker.txt") as file:
+    todos = file.readlines()
+
 # creates the while loop so the functions repeat themselves over and over again 
 while True:  
     print("\nYour current todos are:")
@@ -7,19 +11,32 @@ while True:
     # makes the list of the things to do more ordered 
         print(x)
 
-    newtodos = input("Would you like to add or remove a todo?: ").strip().lower()
+    newtodos = str(input("Would you like to add, remove, or exit?: ")).strip().lower()
+
 
     # the add makes use the append which would add the new thing the user input in the code
     if newtodos == "add": 
         newtodos1 = input("What is your new todo? ").strip()
-        todos.append(newtodos1)
+        todos.append(newtodos1 + "\n")
         print("Todo added!")
-    elif newtodos == "remove":
+    elif newtodos == "exit":
+           # Save all todos to the file before exiting
+
+           with open("todo_tracker.txt","w") as file: 
+             todos = file.writelines(todos)   
+        # Makes sure the code does not repeat itself over and over
+           break   
+    elif newtodos == "remove":     
+    
     # 
         if not todos:
             print("No todos to remove.")
         # continue ends the current iteration and creates a new one 
-            continue
+        
+                        
+        
+            
+           
         
        
         # The del function deletes any new todos by the number minus 1 
